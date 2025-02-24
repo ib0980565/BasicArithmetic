7bff9f57fcc80bd739c8a107d48e0fa42c012331
ib0980565 <ib0980565@otc.edu>
2025-02-24 08:47:19 -0600

    ChangedVariables

---
+++ BasicArithmetic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 5;
+    int sum = a + b;
+    int difference = a - b;
+    int product = a * b;
+    int quotient = a / b;
+    int remainder = a % b;
+
+    cout << "Sum: " << sum << endl;
+    cout << "Difference: " << difference << endl;
+    cout << "Product: " << product << endl;
+    cout << "Quotient: " << quotient << endl;
+    cout << "Remainder: " << remainder << endl;
+    
+    return 0;
+}
